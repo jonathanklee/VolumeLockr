f324506a426574c98de69f9b03e0ba0645d1e17b
Jonathan Klee <jonathan.klee@faurecia.com>
2021-07-11 15:41:35 +0200

    Add simple option menu
    Change-Id: I2c2bca5d4acb2f62da2095a6ac963d7477f1a6e1

--- app/src/main/java/com/klee/volumelockr/MainActivity.kt
+++ app/src/main/java/com/klee/volumelockr/MainActivity.kt
@@ -2,11 +2,19 @@ package com.klee.volumelockr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.options, menu)
+        return true
     }
 }
