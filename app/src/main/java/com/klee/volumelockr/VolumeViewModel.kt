d68533708e64cc47b15ed109729f118213bc7f91
Jonathan Klee <jonathan.klee@e.email>
2021-11-20 22:24:43 +0100

    Add max volumes

--- app/src/main/java/com/klee/volumelockr/VolumeViewModel.kt
+++ app/src/main/java/com/klee/volumelockr/VolumeViewModel.kt
@@ -20,21 +20,27 @@ class VolumeViewModel(application: Application) : AndroidViewModel(application) 
         val resource = mContext.resources
         mVolumes.postValue(listOf(
             Volume(resource.getString(R.string.media_title), AudioManager.STREAM_MUSIC,
-                    fetchVolume(AudioManager.STREAM_MUSIC), 0, 25, false),
+                fetchVolume(AudioManager.STREAM_MUSIC), 0, fetchMaxVolume(AudioManager.STREAM_MUSIC), false),
 
             Volume(resource.getString(R.string.call_title), AudioManager.STREAM_VOICE_CALL,
-                    fetchVolume(AudioManager.STREAM_VOICE_CALL), 1, 7, false),
+                fetchVolume(AudioManager.STREAM_VOICE_CALL), 1, fetchMaxVolume(AudioManager.STREAM_VOICE_CALL), false),
 
             Volume(resource.getString(R.string.notification_title), AudioManager.STREAM_NOTIFICATION,
-                    fetchVolume(AudioManager.STREAM_NOTIFICATION), 0,7, false),
+                fetchVolume(AudioManager.STREAM_NOTIFICATION), 0, fetchMaxVolume(AudioManager.STREAM_NOTIFICATION),
+                false),
 
             Volume(resource.getString(R.string.alarm_title), AudioManager.STREAM_ALARM,
-                    fetchVolume(AudioManager.STREAM_ALARM), 1, 7, false)
+                fetchVolume(AudioManager.STREAM_ALARM), 1, fetchMaxVolume(AudioManager.STREAM_ALARM), false)
         ))
     }
 
     private fun fetchVolume(volume: Int) : Int {
         val audioManager = mContext.getSystemService(Context.AUDIO_SERVICE) as AudioManager
         return audioManager.getStreamVolume(volume)
+    }
+
+    private fun fetchMaxVolume(volume: Int) : Int {
+        val audioManager = mContext.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        return audioManager.getStreamMaxVolume(volume)
     }
 }
