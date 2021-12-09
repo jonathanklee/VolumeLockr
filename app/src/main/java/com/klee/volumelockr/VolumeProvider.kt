f48b062ef3f8729133d3cfc98aeff8d1cd6540be
Jonathan Klee <jonathan.klee@e.email>
2021-12-09 14:07:51 +0100

    Implement VolumeProvider

---
+++ app/src/main/java/com/klee/volumelockr/VolumeProvider.kt
@@ -0,0 +1,35 @@
+package com.klee.volumelockr
+
+import android.content.Context
+import android.media.AudioManager
+
+class VolumeProvider(private val mContext: Context) {
+
+    private val mAudioManager = mContext.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+
+    fun getVolumes() : List<Volume> {
+        val resource = mContext.resources
+        return listOf(
+            Volume(resource.getString(R.string.media_title), AudioManager.STREAM_MUSIC,
+                fetchVolume(AudioManager.STREAM_MUSIC), 0, fetchMaxVolume(AudioManager.STREAM_MUSIC), false),
+
+            Volume(resource.getString(R.string.call_title), AudioManager.STREAM_VOICE_CALL,
+                fetchVolume(AudioManager.STREAM_VOICE_CALL), 1, fetchMaxVolume(AudioManager.STREAM_VOICE_CALL), false),
+
+            Volume(resource.getString(R.string.notification_title), AudioManager.STREAM_NOTIFICATION,
+                fetchVolume(AudioManager.STREAM_NOTIFICATION), 0, fetchMaxVolume(AudioManager.STREAM_NOTIFICATION),
+                false),
+
+            Volume(resource.getString(R.string.alarm_title), AudioManager.STREAM_ALARM,
+                fetchVolume(AudioManager.STREAM_ALARM), 1, fetchMaxVolume(AudioManager.STREAM_ALARM), false)
+        )
+    }
+
+    private fun fetchVolume(volume: Int) : Int {
+        return mAudioManager.getStreamVolume(volume)
+    }
+
+    private fun fetchMaxVolume(volume: Int) : Int {
+        return mAudioManager.getStreamMaxVolume(volume)
+    }
+}
--- app/src/main/java/com/klee/volumelockr/VolumeSliderFragment.kt
+++ app/src/main/java/com/klee/volumelockr/VolumeSliderFragment.kt
@@ -4,7 +4,6 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
-import android.media.AudioManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.IBinder
@@ -24,6 +23,7 @@ class VolumeSliderFragment : Fragment() {
 
     private lateinit var mRecyclerView: RecyclerView
     private lateinit var mAdapter: VolumeAdapter
+    private lateinit var mVolumeProvider: VolumeProvider
     private var mService: VolumeService? = null
 
     override fun onCreateView(
@@ -36,6 +36,8 @@ class VolumeSliderFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        mVolumeProvider = VolumeProvider(requireContext())
 
         Intent(context, VolumeService::class.java).also { intent ->
             context?.bindService(intent, connection, Context.BIND_AUTO_CREATE)
@@ -70,35 +72,8 @@ class VolumeSliderFragment : Fragment() {
     private fun setupRecyclerView(service: VolumeService?) {
         mRecyclerView = requireView().findViewById(R.id.recycler_view)
         mRecyclerView.layoutManager = LinearLayoutManager(context)
-        mAdapter = VolumeAdapter(buildVolumesFromSettings(), service, requireContext())
+        mAdapter = VolumeAdapter(mVolumeProvider.getVolumes(), service, requireContext())
         mRecyclerView.adapter = mAdapter
-    }
-
-    private fun buildVolumesFromSettings() : List<Volume> {
-        return listOf(
-            Volume(resources.getString(R.string.media_title), AudioManager.STREAM_MUSIC,
-                fetchVolume(AudioManager.STREAM_MUSIC), 0, fetchMaxVolume(AudioManager.STREAM_MUSIC), false),
-
-            Volume(resources.getString(R.string.call_title), AudioManager.STREAM_VOICE_CALL,
-                fetchVolume(AudioManager.STREAM_VOICE_CALL), 1, fetchMaxVolume(AudioManager.STREAM_VOICE_CALL), false),
-
-            Volume(resources.getString(R.string.notification_title), AudioManager.STREAM_NOTIFICATION,
-                fetchVolume(AudioManager.STREAM_NOTIFICATION), 0, fetchMaxVolume(AudioManager.STREAM_NOTIFICATION),
-                false),
-
-            Volume(resources.getString(R.string.alarm_title), AudioManager.STREAM_ALARM,
-                fetchVolume(AudioManager.STREAM_ALARM), 1, fetchMaxVolume(AudioManager.STREAM_ALARM), false),
-        )
-    }
-
-    private fun fetchVolume(volume: Int) : Int {
-        val audioManager = context?.getSystemService(Context.AUDIO_SERVICE) as AudioManager
-        return audioManager.getStreamVolume(volume)
-    }
-
-    private fun fetchMaxVolume(volume: Int) : Int {
-        val audioManager = context?.getSystemService(Context.AUDIO_SERVICE) as AudioManager
-        return audioManager.getStreamMaxVolume(volume)
     }
 
     private val connection = object : ServiceConnection {
@@ -109,7 +84,7 @@ class VolumeSliderFragment : Fragment() {
             setupRecyclerView(mService)
 
             mService?.registerOnVolumeChangeListener(Handler(Looper.getMainLooper())) {
-                mAdapter.update(buildVolumesFromSettings())
+                mAdapter.update(mVolumeProvider.getVolumes())
             }
 
             mService?.registerOnModeChangeListener {
