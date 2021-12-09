20afcd34d4d6f73a4d3586bad8cd6a5b5a558907
Jonathan Klee <jonathan.klee@e.email>
2021-12-09 08:58:20 +0100

    Re-implement max volumes

--- app/src/main/java/com/klee/volumelockr/VolumeSliderFragment.kt
+++ app/src/main/java/com/klee/volumelockr/VolumeSliderFragment.kt
@@ -77,22 +77,28 @@ class VolumeSliderFragment : Fragment() {
     private fun buildVolumesFromSettings() : List<Volume> {
         return listOf(
             Volume(resources.getString(R.string.media_title), AudioManager.STREAM_MUSIC,
-                fetchVolume(AudioManager.STREAM_MUSIC), 0, 25, false),
+                fetchVolume(AudioManager.STREAM_MUSIC), 0, fetchMaxVolume(AudioManager.STREAM_MUSIC), false),
 
             Volume(resources.getString(R.string.call_title), AudioManager.STREAM_VOICE_CALL,
-                fetchVolume(AudioManager.STREAM_VOICE_CALL), 1, 7, false),
+                fetchVolume(AudioManager.STREAM_VOICE_CALL), 1, fetchMaxVolume(AudioManager.STREAM_VOICE_CALL), false),
 
             Volume(resources.getString(R.string.notification_title), AudioManager.STREAM_NOTIFICATION,
-                fetchVolume(AudioManager.STREAM_NOTIFICATION), 0,7, false),
+                fetchVolume(AudioManager.STREAM_NOTIFICATION), 0, fetchMaxVolume(AudioManager.STREAM_NOTIFICATION),
+                false),
 
             Volume(resources.getString(R.string.alarm_title), AudioManager.STREAM_ALARM,
-                fetchVolume(AudioManager.STREAM_ALARM), 1, 7, false),
+                fetchVolume(AudioManager.STREAM_ALARM), 1, fetchMaxVolume(AudioManager.STREAM_ALARM), false),
         )
     }
 
     private fun fetchVolume(volume: Int) : Int {
         val audioManager = context?.getSystemService(Context.AUDIO_SERVICE) as AudioManager
         return audioManager.getStreamVolume(volume)
+    }
+
+    private fun fetchMaxVolume(volume: Int) : Int {
+        val audioManager = context?.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        return audioManager.getStreamMaxVolume(volume)
     }
 
     private val connection = object : ServiceConnection {
